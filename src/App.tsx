3f1d3f63e350ba083333adb748aec63784044ee1
eliyasmd50 <eliyasmohamed50@gmail.com>
2025-05-30 01:43:48 +0530

    initial commit for header module

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
-import { ReactHTMLElement } from 'react';
-import './App.css';
+import './App.css'
 
-function App(){
+function App() {
+
   return (
-      <header className='App-header'>
-        <h1>Portfolio website</h1>
-      </header>
-  );
+    <>
+     <header className='app-header'>
+      <h1>Portfolio website</h1>
+     </header>
+    </>
+  )
 }
 
-export default App;
+export default App
