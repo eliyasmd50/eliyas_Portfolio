6f1484c80b9e7b7ec2280d8e768835dfad8f81d2
eliyasmd50 <eliyasmohamed50@gmail.com>
2026-03-01 17:41:38 +0530

    bug fixes

--- src/components/SocialButtons.tsx
+++ src/components/SocialButtons.tsx
@@ -3,7 +3,10 @@ import { FaGithub, FaLinkedin, FaEnvelope  } from "react-icons/fa";
 const SocialButtons = () => {
   return (
     <div style={{ display: "flex", gap: "15px" }} className="hero-buttons">
-    <a href="./src/Resume.pdf" download="Mohamed_Eliyas_Resume.pdf" className="cv-btn">
+    <a 
+      href="resume.pdf"
+      download="Mohamed_Eliyas_Resume.pdf" 
+      className="cv-btn">
         Download CV
         <svg viewBox="0 0 24 24" width="18" height="18" fill="none" stroke="currentColor" stroke-width="2">
             <path d="M12 4v12"></path>
